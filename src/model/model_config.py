4f2d7f485ba4b9b20627d2fd028e8f8fe9b11f34
ziul-bio <ziul.bio@gmail.com>
2025-05-27 15:39:19 -0500

    fixing resume training

---
+++ src/model/model_config.py
@@ -0,0 +1,10 @@
+
+
+
+config = {
+    "model_checkpoint": 'esmc_300m', 
+    "learning_rate": 5e-4,
+    "weight_decay": 1e-2,
+    "beta1": 0.9,
+    "beta2": 0.95,
+  }
--- src/training/train.py
+++ src/training/train.py
@@ -1,34 +1,32 @@
 import torch
 import torch.nn as nn
 from torch.optim import AdamW 
-from torch.optim.lr_scheduler import SequentialLR, LinearLR, ConstantLR, CosineAnnealingLR
+from torch.optim.lr_scheduler import SequentialLR, LinearLR, ConstantLR, CosineAnnealingLR, ReduceLROnPlateau
 
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger 
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 
 # my modules
-from src.model.ESMC import Load_from_pretrained
+from src.model.ESMC2ne import Load_from_pretrained
 from src.data.dataset import MyDataModule
 torch.set_float32_matmul_precision('medium')
 from pytorch_lightning.tuner.tuning import Tuner
 
-from src.model.model_config import config2
+from src.model.model_config import config
 
 
 ################ pytorch lightning model ######################
 class LitModel(pl.LightningModule):
-    def __init__(self, args):
+    def __init__(self):
         super().__init__()
-        if not args.resume:
-            self.model= Load_from_pretrained(args.checkpoint_path).get_model_details()
-        else:
-            self.model = LitModel.load_from_checkpoint(args.checkpoint_resume)
-        
-        self.learning_rate = args.learning_rate
-        self.weight_decay = args.weight_decay
-        self.beta1 = args.beta1
-        self.beta2 = args.beta2
+
+        self.model = Load_from_pretrained(config['model_checkpoint']).get_model_details()
+
+        self.learning_rate = config['learning_rate']
+        self.weight_decay = config['weight_decay']
+        self.beta1 = config['beta1']
+        self.beta2 = config['beta2']
         
         # metrics and loss function
         pad_token_id = 1
@@ -47,7 +45,7 @@ class LitModel(pl.LightningModule):
         logits = self(batch)
         targets = batch['labels']
         loss = self.loss_fn(logits.view(-1, logits.size(-1)), targets.view(-1))
-        self.log("train_loss", loss, on_step=True, on_epoch=False, prog_bar=True, logger=True, sync_dist=True)
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
         return loss
  
     def validation_step(self, batch):
@@ -69,22 +67,23 @@ class LitModel(pl.LightningModule):
         return loss, perplexity
 
 
-    # def configure_optimizers(self):
-    #     Optimizer = AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay, betas=(self.beta1, self.beta2))
-    #     LRscheduler = CosineAnnealingLR(Optimizer, T_max=10, eta_min=self.learning_rate*0.1),
-    #     LRscheduler = ReduceLROnPlateau(Optimizer, mode='min', factor=0.9, patience=1, cooldown=0),
-    #     return {"optimizer": Optimizer, "lr_scheduler": LRscheduler}    
-    #     #return Optimizer
-        
     def configure_optimizers(self):
-        warmup_iters = 10000
-        tmax = 80000
-        optimizer = AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay, betas=(self.beta1, self.beta2))
-        warmup = LinearLR(optimizer, start_factor=0.1, total_iters=warmup_iters)                   # total iterations that warmup will last. warms up to 0.01*learning_rate
-        decay = ConstantLR(optimizer, factor=1.0, total_iters=tmax)
-        #decay = CosineAnnealingLR(optimizer, T_max=tmax, eta_min=self.learning_rate * 0.1)          # T_max is the number of iterations for cosine annealing. decays to 0.01*learning_rate
-        scheduler = SequentialLR(optimizer, schedulers=[warmup, decay], milestones=[warmup_iters])  # milestones means when to switch from warm up to lr decay
-        return {"optimizer": optimizer, "lr_scheduler": scheduler}
+        Optimizer = AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay, betas=(self.beta1, self.beta2))
+        #return Optimizer
+        #LRscheduler = CosineAnnealingLR(Optimizer, T_max=10, eta_min=self.learning_rate*0.1),
+        #return {"optimizer": Optimizer, "lr_scheduler": LRscheduler}    
+        LRscheduler = ReduceLROnPlateau(Optimizer, mode='min', factor=0.9, patience=1, cooldown=0) # It decays exponentially, by 10% each time it's triggered.
+        return {"optimizer": Optimizer, "lr_scheduler": {"scheduler": LRscheduler, "monitor": "val_loss"}}    
+        
+    # def configure_optimizers(self):
+    #     warmup_iters = 10000
+    #     tmax = 80000
+    #     optimizer = AdamW(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay, betas=(self.beta1, self.beta2))
+    #     warmup = LinearLR(optimizer, start_factor=0.1, total_iters=warmup_iters)                     # total iterations that warmup will last. warms up to 0.01*learning_rate
+    #     decay = ConstantLR(optimizer, factor=1.0, total_iters=tmax)
+    #     #decay = CosineAnnealingLR(optimizer, T_max=tmax, eta_min=self.learning_rate * 0.1)          # T_max is the number of iterations for cosine annealing. decays to 0.01*learning_rate
+    #     scheduler = SequentialLR(optimizer, schedulers=[warmup, decay], milestones=[warmup_iters])   # milestones means when to switch from warm up to lr decay
+    #     return {"optimizer": optimizer, "lr_scheduler": scheduler}
 
 
 
@@ -93,10 +92,9 @@ def main(args):
     dataset_name = args.dataDir.split('/')[-2]
     print(f"Dataset name: {dataset_name}")
     datamodule = MyDataModule(args)
-
-    print("Loading model...")
-    model = LitModel(args)
-    
+    print('Loading model...')
+    model = LitModel()
+ 
 
     ########################### Training setup ##########################
     logger = TensorBoardLogger(
@@ -108,6 +106,7 @@ def main(args):
     checkpoint_callback = ModelCheckpoint(
         dirpath=f'checkpoints/{args.output}/',  
         filename="{epoch}-{val_loss:.2f}",  
+        save_on_train_epoch_end=True,
         monitor="val_loss",               
         save_top_k=2,                      
         mode="min",                         # Mode for monitoring (min for loss, max for accuracy, etc.)
@@ -115,7 +114,7 @@ def main(args):
     
     early_stopping_callback = EarlyStopping(
         monitor='val_loss',
-        patience=50,                        # I am looking for 5 epochs, so patience=5*number of checks per epoch
+        patience=5,                          # I am looking for 5 epochs, so patience=5*number of checks per epoch
         mode='min',
         min_delta=0.001,
     )
@@ -126,7 +125,7 @@ def main(args):
         devices=[3], #[0, 1, 2, 3],          # [0, 1] for 2 GPUs, or -1 for all available GPUs
         #accumulate_grad_batches=100,          # simulate a × larger batch size (so 3x4=16)
         max_epochs= args.epochs,                 
-        val_check_interval=2000,
+        #val_check_interval=5000,
         enable_checkpointing=True,
         gradient_clip_val=1.0,               # Clip gradients if they exceed 1.0
         logger=logger,
@@ -140,13 +139,13 @@ def main(args):
         fig = lr_finder.plot(suggest=True)                                                                     # type: ignore
         lr_suggestion = round(lr_finder.suggestion(), 10)                                                      # type: ignore
         print(f"Suggested learning rate: {lr_suggestion}")
-        fig.savefig(f"logs/{args.output}/LRfind_{lr_suggestion}.png")                                          # type: ignore
+        fig.savefig(f"logs/{config['output']}/LRfind_{lr_suggestion}.png")                                          # type: ignore
 
     
     ########################### Training ##########################    
     if args.resume:
-        print(f"Resuming training from {args.checkpoint}!")
-        trainer.fit(model, datamodule, ckpt_path=args.checkpoint)
+        print(f"Resuming training from {args.checkpoint_resume}!")
+        trainer.fit(model, datamodule, ckpt_path=args.checkpoint_resume)
         trainer.test(model, datamodule=datamodule)
 
     elif not args.LRfinder:
@@ -161,16 +160,12 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-i', '--dataDir', type=str, default='data/processed/C-RVDBv29_no_poly_20aa/')
     parser.add_argument('-o', '--output', type=str, default=None)
-    parser.add_argument('--batch_size', type=int, default=6)
-    parser.add_argument('--epochs', type=int, default=5)
-    parser.add_argument('--learning_rate', type=float, default=1e-6)
-    parser.add_argument('--weight_decay', type=float, default=1e-6)
-    parser.add_argument('--beta1', type=float, default=0.9)
-    parser.add_argument('--beta2', type=float, default=0.99)
-    parser.add_argument('--checkpoint_path', type=str, default='/stor/work/Wilke/wilkelab/pLMs_checkpoints/ESMC/esmc_300m_2024_12_v0.pth')
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--epochs', type=int, default=40)
+    parser.add_argument('--model_checkpoint', type=str, default='esmc_300m')
+    parser.add_argument('--LRfinder', action='store_true')
     parser.add_argument('--resume', action='store_true')
     parser.add_argument('--checkpoint_resume', type=str)
-    parser.add_argument('--LRfinder', action='store_true')
     args = parser.parse_args()
     main(args)
 
@@ -178,5 +173,6 @@ if __name__ == '__main__':
 
 
 ###### RUNNING EXAMPLES ######  
-# # this version I reduced the weight decay to 1e-2  
-# CUDA_VISIBLE_DEVICES=2 python src/training/train.py -o ViCAM_300M/test_lr_finder --LRfinder
+# python src/training/train.py -o ViCAM_300M/test_lr_finder --LRfinder
+# python src/training/train.py -o logs/ViCAM_300M/CRVDBv29_noPoly_Frz3_lr5e4_RLRP_v02
+# python src/training/train.py -o logs/ViCAM_300M/CRVDBv29_noPoly_Frz3_lr5e4_RLRP --resume --checkpoint_resume checkpoints/ViCAM_300M/CRVDBv29_noPoly_Frz3_lr5e4_RLRP/epoch=9-val_loss=1.46.ckpt  
