e0a9b86acce69dc6c443353ecf2fd4c68fa48410
ziul-bio <ziul.bio@gmail.com>
2025-07-25 09:56:33 -0500

    adding esm2 config file

---
+++ src/model/esm2_config.py
@@ -0,0 +1,8 @@
+config = {
+    #"model_checkpoint": '/stor/work/Wilke/luiz/ViCAM/checkpoints/ESM-2/esm2_t33_650M_UR50D.pt', 
+    "model_checkpoint": 'esm2_t33_650M_UR50D', 
+    "learning_rate": 4e-4,
+    "weight_decay": 1e-2,
+    "beta1": 0.9,
+    "beta2": 0.95,
+  }
